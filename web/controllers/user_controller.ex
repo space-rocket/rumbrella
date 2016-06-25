eff88b5ff9a205e17d0eba603577f1712799046d
Michael Chavez <bigbassroller@gmail.com>
2016-06-25 07:15:58 -0700

    authentication plug refactor

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,15 +1,11 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Rumbl.User
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(User)
+    render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
@@ -34,7 +30,7 @@ defmodule Rumbl.UserController do
   	end
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
