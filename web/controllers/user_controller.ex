0e85b783307062aa4b8e2942bd2a2af16a013936
Michael Chavez <bigbassroller@gmail.com>
2016-06-25 22:32:52 -0700

    added video module

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -31,14 +31,4 @@ defmodule Rumbl.UserController do
   	end
   end
 
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
 end
