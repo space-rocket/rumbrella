86cd0e6288fb7eb21f6c8bd4322c848bd6cbee9b
Michael Chavez <bigbassroller@gmail.com>
2016-06-26 21:40:02 -0700

    video controller test passing in for logging in user

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,18 +1,24 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
-  test "requires user authentication on all actions", %{conn: conn} do
-    Enum.each([
-      get(conn, video_path(conn, :new)),
-      get(conn, video_path(conn, :index)),
-      get(conn, video_path(conn, :show, "123")),
-      get(conn, video_path(conn, :edit, "123" )),
-      put(conn, video_path(conn, :update, "123", %{})),
-      get(conn, video_path(conn, :create, %{})),
-      delete(conn, video_path(conn, :delete, "123")),
-      ], fn conn ->
-        assert html_response(conn, 302)
-        assert conn.halted
-      end)
+  setup %{conn: conn} = config do
+    if username =config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user} 
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "max"
+  test "lists all user's videos on index", %{conn: conn, user: user} do
+    user_video  = insert_video(user, title: "funny cats")
+    other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+    conn = get conn, video_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing videos/
+    assert String.contains?(conn.resp_body, user_video.title)
+    refute String.contains?(conn.resp_body, other_video.title)
   end
 end
