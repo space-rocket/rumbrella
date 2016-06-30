bd950e355f3818459f12ec566de322b57ed7d57d
Michael Chavez <bigbassroller@gmail.com>
2016-06-30 16:38:31 -0700

    added annotation persistance test

--- apps/rumbl/test/channels/video_channel_test.exs
+++ apps/rumbl/test/channels/video_channel_test.exs
@@ -22,4 +22,21 @@ defmodule Rumbl.Channels.VideoChannelTest do
 		assert socket.assigns.video_id == vid.id
 		assert %{annotations: [%{body: "one"}, %{body: "two"}]} = reply
 	end
+
+	test "inserting new annotations", %{socket: socket, video: vid} do
+		{:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+		ref = push socket, "new_annotation", %{body: "the body", at: 0}
+		assert_reply ref, :ok, %{}
+		assert_broadcast "new_annotation", %{}
+	end
+
+	test "new annotations triggers InfoSys", %{socket: socket, video: vid} do
+		insert_user(username: "wolfram")
+		{:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+		ref = push socket, "new_annotation", %{body: "1 + 1", at: 123}
+		assert_reply ref, :ok, %{}
+		assert_broadcast "new_annotation", %{body: "1 + 1", at: 123}
+		assert_broadcast "new_annotation", %{body: "2", at: 123}
+	end
+
 end
